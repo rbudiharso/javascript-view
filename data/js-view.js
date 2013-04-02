72e82d9dfdbde351182c10e70caee2098998a6ea
Rahmat Budiharso <rbudiharso@gmail.com>
2013-04-02 22:45:56 +0700

    fix bug https://github.com/rahmat-budiharso/javascript-view/issues/1

--- data/js-view.js
+++ data/js-view.js
@@ -1,45 +1,50 @@
 self.port.on("processSource", function() {
-  var body = document.body,
-  pre1 = body.children[0],
-  pre2 = document.createElement('pre'),
-  code = document.createElement('code'),
-  lineHeight, lineCount, lineNumberWrapper;
+    var content = document.contentType;
+    if (content === 'application/javascript' || content === 'application/x-javascript' || content === 'text/plain') {
+        var body = document.body,
+            pre1 = body.children[0],
+            pre2 = document.createElement('pre'),
+            code = document.createElement('code'),
+            lineHeight, lineCount, lineNumberWrapper, linkStyle;
 
-  body.style.margin = 0;
-  code.style.wordWrap = 'normal';
+        body.style.margin = 0;
+        code.style.wordWrap = 'normal';
 
-  code.setAttribute('data-language', 'javascript');
-  code.textContent = js_beautify(pre1.textContent, {
-    'unescape_strings': true,
-    'jslint_happy': true,
-    'brace_style': 'collapse',
-    'break_chained_methods': false,
-    'indent_scripts': 'normal',
-    'indent_size': 4,
-    'keep_array_indentation': false,
-    'preserve_newlines': true,
-    'space_after_anon_function': true,
-    'space_before_conditional': true
-  });
+        pre2.setAttribute('class', 'jsv');
 
-  pre2.appendChild(code);
+        code.setAttribute('data-language', 'javascript');
+        code.textContent = js_beautify(pre1.textContent, {
+            'unescape_strings': true,
+            'jslint_happy': true,
+            'brace_style': 'collapse',
+            'break_chained_methods': false,
+            'indent_scripts': 'normal',
+            'indent_size': 4,
+            'keep_array_indentation': false,
+            'preserve_newlines': true,
+            'space_after_anon_function': true,
+            'space_before_conditional': true
+        });
 
-  body.replaceChild(pre2, pre1);
-  Rainbow.color();
+        pre2.appendChild(code);
 
-  lineHeight = parseInt(window.getComputedStyle(code, null).lineHeight, 10);
-  lineCount = Math.ceil(code.offsetHeight / lineHeight);
+        body.replaceChild(pre2, pre1);
+        Rainbow.color();
 
-  lineNumberWrapper = document.createElement('ol');
-  lineNumberWrapper.setAttribute('class', 'line-number');
+        lineHeight = parseInt(window.getComputedStyle(code, null).lineHeight, 10);
+        lineCount = Math.ceil(code.offsetHeight / lineHeight);
 
-  for (var i = 0; i < lineCount; i++) {
-    var lineNumber = document.createElement('li');
-    lineNumber.textContent = i + 1;
-    lineNumberWrapper.appendChild(lineNumber);
-  }
-  pre2.insertBefore(lineNumberWrapper, pre2.firstChild);
+        lineNumberWrapper = document.createElement('ol');
+        lineNumberWrapper.setAttribute('class', 'line-number');
 
-  var lineNumberWidth = lineNumberWrapper.lastChild.offsetWidth;
-  pre2.style.paddingLeft = (lineNumberWidth + 20) + 'px';
+        for (var i = 0; i < lineCount; i++) {
+            var lineNumber = document.createElement('li');
+            lineNumber.textContent = i + 1;
+            lineNumberWrapper.appendChild(lineNumber);
+        }
+        pre2.insertBefore(lineNumberWrapper, pre2.firstChild);
+
+        var lineNumberWidth = lineNumberWrapper.lastChild.offsetWidth;
+        pre2.style.paddingLeft = (lineNumberWidth + 20) + 'px';
+    }
 });
